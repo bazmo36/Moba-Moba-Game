0ea0a26944a5ca0aaa2614962ef69f31615965e3
bazmo36 <mnalqallaf362@gmail.com>
2025-07-13 09:54:03 +0300

    submit my work

---
+++ js/app.js
@@ -0,0 +1,150 @@
+function init() {
+
+    /*-------------- Constants -------------*/
+    const cells = []
+    const gridWidth = 10
+    const numberOfCells = gridWidth * gridWidth
+
+    /*---------- Variables (state) ---------*/
+    let score = 0
+    // let highscore = 0
+    let monkeyPosition = 95
+    let lives = 3
+    let itemType = null
+    let fallingItem = null
+    let fallInterval
+
+
+
+    /*----- Cached Element References  -----*/
+    const gridElem = document.querySelector('.grid')
+    const scoreElem = document.querySelector('#score')
+    const livesElem = document.querySelector('#lives')
+    const leftBtnEl = document.querySelector('#left-btn')
+    const rightBtnEl = document.querySelector('#right-btn')
+    // const highScoreEl = document.querySelector('#hi-score')
+
+    /*-------------- Functions -------------*/
+
+    function render() {
+        cells.forEach(cell => cell.classList.remove('monkey', 'banana', 'golden', 'rotten', 'bomb'))
+        cells[monkeyPosition].classList.add('monkey')
+        if (fallingItem !== null) {
+            cells[fallingItem].classList.add(itemType)
+        }
+
+        let hearts = '';
+        for (let i = 0; i < lives; i++) {
+            hearts += '❤️';
+        }
+        livesElem.textContent = hearts;
+
+        scoreElem.textContent = score
+
+    }
+
+    function createGrid() {
+        for (let i = 0; i < numberOfCells; i++) {
+            const cell = document.createElement('div')
+            cell.textContent = i
+            cell.classList.add('cell');
+            gridElem.appendChild(cell);
+            cells.push(cell);
+        }
+    }
+
+    function moveMonkey(direction) {
+        cells.monkeyPosition.classList.remove('monkey')
+        if (direction === 'left' && monkeyPosition % gridWidth !== 0) {
+            monkeyPosition--
+        } else if (direction === 'right' && monkeyPosition % gridWidth !== 9) {
+            monkeyPosition++
+        }
+        render()
+    }
+
+
+    function pickItem() {
+        const types = ['banana', 'banana', 'banana', 'banana', 'golden', 'rotten', 'bomb']
+        if (lives < 3) types.push('heart')
+        const randIndex = Math.floor(Math.random() * types.length)
+        itemType = types[randIndex]
+        fallingItem = Math.floor(Math.random() * gridWidth)
+
+
+    }
+
+    function dropItem() {
+        if (fallingItem !== null && fallingItem < numberOfCells - gridWidth) {
+            fallingItem += gridWidth
+        } else {
+            if (fallingItem === monkeyPosition) {
+                handleCatch(itemType)
+            }
+            pickItem()
+        }
+        render()
+    }
+
+    function handleCatch(type) {
+        switch (type) {
+            case 'banana':
+                score += 5
+                break
+            case 'golden':
+                score += 50
+                break
+            case 'rotten':
+                if (score > 0) {
+                    score -= 10
+                }
+                if (score === 0) {
+                    score = 0
+                }
+                break
+            case 'bomb':
+                lives--
+                break
+            case 'heart':
+                if (lives < 3) lives++
+                break
+        }
+
+        if (lives <= 0) {
+            resetGame()
+        }
+    }
+
+    function startGame() {
+        pickItem()
+        fallInterval = setInterval(dropItem, 500)
+    }
+
+    function gameOver() {
+
+    }
+
+
+    function resetGame() {
+        clearInterval(fallInterval)
+        monkeyPosition = 95
+        score = 0
+        lives = 3
+        fallingItem = null
+        pickItem()
+        startGame()
+    }
+
+    /*----------- Event Listeners ----------*/
+
+    leftBtnEl.addEventListener('click', () => moveMonkey('left'))
+    rightBtnEl.addEventListener('click', () => moveMonkey('right'))
+
+    // init game
+    createGrid()
+    pickItem()
+    render()
+}
+
+
+document.addEventListener('DOMContentLoaded', init)
